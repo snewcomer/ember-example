40bea42b750363d36ed8c3bd430f5c58b0a5180c
snewcomer <snewcomer24@gmail.com>
2016-08-31 09:53:19 -0700

    init commit frontend

--- backend/src/ticket/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from ticket.models import Ticket
-
-
-class TicketSerializer(serializers.HyperlinkedModelSerializer):
-
-    class Meta:
-        model = Ticket
-        fields = ('request',)
